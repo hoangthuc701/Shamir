cf05cf5f560d406dc230f7f9a7c7a08f97b57da6
Hoang Thuc <hoangthuc701@gmail.com>
2021-11-22 20:16:17 +0700

    finish 1

--- Shamir.py
+++ Shamir.py
@@ -1,21 +1,44 @@
 import random
 import sys
 import numpy as np
+from sklearn.utils import shuffle
 
 class Shamir:
-    def extract(self,k, numbers):
-        a = np.zeros((k,k+1))
-        for i in range(1,k+1):
-            for j in range(k,0,-1):
-                a[i-1,k-j] = pow(i,j-1)
-            a[i-1][k] = numbers[i-1]
-        coefficients =  self.gaussSolver(k,a)
+    def join(self,xs,ys,k):
+        '''
+        Tái tạo tin mật s từ n' phần của tin mật (n' >= k).
 
+        Các tham số:
+        xs, ys (2 mảng numpy một chiều, len = n'): n' phần của tin mật: phần thứ 1 là (xs[0], ys[0]), 
+                                                                        phần thứ 2 là (xs[1], ys[1]), 
+                                                                        ...
+        k (int): Ngưỡng k mà đã dùng khi phân chia tin mật.
+        Giá trị trả về:
+            int: Tin mật s được tái tạo,
+                trong trường hợp không đủ số phần để tái tạo tin mật thì trả về None.        
+
+        '''
+        if len(xs) < k:
+            return None
+
+        matrix = np.zeros((k,k+1))
+        for i in range(0,min(k, len(xs))):
+            for j in range(k,0,-1):
+                matrix[i,k-j] = pow(xs[i],j-1)
+            matrix[i][k] = ys[i]
+        try: 
+            coefficients =  self.solver(k,matrix)
+        except:
+            return None
         return int(round(coefficients[k-1]))
 
-    def gaussSolver(self, n, matrix):
+    def solver(self, n, matrix):
+        '''
+        Hàm giải ma hàm n ẩn với n phương trình
+        
+        '''
         a = matrix.astype(float)
-        x = np.zeros(n+1)
+        coefs = np.zeros(n+1)
         for i in range(n):
             if a[i][i] == 0.0:
                 sys.exit('Divide by zero detected!')
@@ -27,19 +50,35 @@ class Shamir:
                     a[j][k] = a[j][k] - ratio * a[i][k]
 
         # Back Substitution
-        x[n-1] = a[n-1][n]/a[n-1][n-1]
+        coefs[n-1] = a[n-1][n]/a[n-1][n-1]
 
         for i in range(n-2,-1,-1):
-            x[i] = a[i][n]
+            coefs[i] = a[i][n]
             
             for j in range(i+1,n):
-                x[i] = x[i] - a[i][j]*x[j]
+                coefs[i] = coefs[i] - a[i][j]*coefs[j]
             
-            x[i] = x[i]/a[i][i]
+            coefs[i] = coefs[i]/a[i][i]
         
-        return x
+        return coefs
 
-    def create(self,s,n,k):
+    def split(self,s,n,k):
+        '''
+        Hàm phân chia tin mật s thành n phần sao cho chỉ khi có ít nhất là k phần (k<=n)
+        thì mới tái tạo được s, còn ít hơn k phần thì sẽ không biết gì về s.
+
+        Các tham số:
+        s (float): Tin mật cần chia sẻ.
+        n (int): Số phần cần phân chia.
+        k (int): Ngưỡng k (k <= n).
+        Giá trị trả về:
+        2 mảng numpy một chiều: Gọi 2 mảng này là xs và ys,
+                                n phần được phân chia: phần thứ 1 là (xs[0], ys[0]),
+                                                       phần thứ 2 là (xs[1], ys[1]),
+                                                       ...
+                                với ys[i] = f(xs[i]).
+
+        '''
         if k >= n:
             return
         coefficients = [0]*(k-1)
@@ -52,32 +91,50 @@ class Shamir:
             coefficients[i] = value
         
         coefficients.append(s)
-
-        print(coefficients)
         
         # generate share key
-        results = []
+        xs = []
+        ys = []
         for i in range(1,n+1):
-            result = self.evaluate(i,coefficients)
-            results.append(result)
+            result = self.compute_polynomial(i,coefficients)
+            xs.append(i)
+            ys.append(result)
 
-        return results
+        return xs,ys
 
-    def evaluate(self,number,coefficients):
+    def compute_polynomial(self,xs,coefficients):
+        '''
+        Tính giá trị của hàm đa thức.
+        '''
         result = 0
         n = len(coefficients) - 1
         for i in range(0, len(coefficients)):
-            result = result + coefficients[i]*pow(number,n)
+            result = result + coefficients[i]*xs**n
             n = n - 1 
         return result
 
-
     def random(self):
-        return random.randint(0,10)
+        return random.randint(0,100)
+
+# Phân chia tin mật 
+s= 789
+n= 10
+k= 1
 
 shamir = Shamir()
-s= 7899
-n= 10
-k= 9
-x = shamir.create(7899,n,k)
-print(shamir.extract(k,x)==s)
+xs, ys = shamir.split(s,n,k)
+
+# Chọn ngẫu nhiên n' phần để tái tạo tin mật
+
+xs1, ys1 = shuffle(xs, ys, random_state = 0)
+n_prime = k - 1
+rec_s = shamir.join(xs1[:n_prime], ys1[:n_prime], k)
+print(rec_s)
+
+n_prime = k
+rec_s1 = shamir.join(xs1[:n_prime], ys1[:n_prime], k)
+print(rec_s1)
+
+n_prime = k + 1
+rec_s2 = shamir.join(xs1[:n_prime], ys1[:n_prime], k)
+print(rec_s2)
